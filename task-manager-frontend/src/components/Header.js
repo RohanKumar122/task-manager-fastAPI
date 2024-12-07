a5eeb8a30ace58039f7682f0936e3dac28a38be3
Rohan Kumar <rk399504@gmail.com>
2024-12-07 21:25:48 +0530

    Update logo

--- task-manager-frontend/src/components/Header.js
+++ task-manager-frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "../utils/images/logo.png";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,7 +13,10 @@ const Header = () => {
 
   return (
     <header className="bg-blue-500 text-white p-4 flex items-center justify-between relative">
+      <div className="flex items-center">
+      <img src={logo} alt="Logo" className="w-6 h-6 mx-2" />
       <h1 className="text-xl">Task Manager</h1>
+      </div>
       <div className="relative">
         {/* Hamburger Button */}
         <button
@@ -37,9 +41,19 @@ const Header = () => {
             </button>
             <button
               onClick={() => handleNavigation("/login")}
-              className="block px-4 py-2 w-full text-left hover:bg-gray-100"
+              className="block px-4 py-2 w-full text-left text-green-600 hover:bg-gray-100"
             >
               Login
+            </button>
+
+            <button
+              onClick={() => {
+                localStorage.removeItem("token"); 
+                window.location.href = "/login"; 
+              }}
+              className="text-red-500 hover:text-red-700 block px-4 py-2 w-full text-left"
+            >
+              Logout
             </button>
           </div>
         )}
